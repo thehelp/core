3b451b988542db467eb2a37243448fefdc250d69
Scott Nonnenberg <scott@nonnenberg.com>
2013-11-25 16:11:34 -0800

    removing unneeded/duplicate configuration

--- src/client/config.js
+++ src/client/config.js
@@ -28,10 +28,6 @@ define(function() {
 
       winston: 'src/client/shims/winston_shim',
       util: 'src/client/shims/util_shim',
-
-      'thehelp-test': 'lib/vendor/thehelp-test',
-      'thehelp-test-coverage': 'lib/vendor/thehelp-test-coverage',
-      'grunt-mocha-bridge': 'lib/vendor/grunt-mocha-bridge'
     },
     shim: {
       'timezone-js': { exports: 'window.timezoneJS' }
--- test/integration/client/dist_config.js
+++ test/integration/client/dist_config.js
@@ -12,7 +12,6 @@ define(function() {
       winston: 'src/client/shims/winston_shim',
       util: 'src/client/shims/util_shim',
 
-      jquery: 'lib/vendor/jquery',
       'thehelp-test': 'lib/vendor/thehelp-test',
       'thehelp-test-coverage': 'lib/vendor/thehelp-test-coverage',
       'grunt-mocha-bridge': 'lib/vendor/grunt-mocha-bridge'
