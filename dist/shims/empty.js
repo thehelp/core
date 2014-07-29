8345ddcc15b27dc2934a1ab0ceaf8b4f4985d118
Scott Nonnenberg <scott@nonnenberg.com>
2014-07-28 20:12:35 -0700

    Remove dist and docs from repo

--- dist/shims/empty.js
+++
@@ -1,8 +0,0 @@
-// Used for things that are `require()`'d on on the client side but never used.
-// For example, [`ModelHelpers`](../../../both/model_helpers.html) loads
-// `jugglingdb-postgres` on the client but actually uses a web-appropriate adapter.
-define(function() {
-  'use strict';
-
-  return {};
-});
--- dist/shims/empty3.js
+++
@@ -1,6 +0,0 @@
-// See the documentation for [`empty2`](empty2.html)
-define(function() {
-  'use strict';
-
-  return {};
-});
